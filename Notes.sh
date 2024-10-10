36c8ee5b3655a8e39ae19b3b39fa2204fcc73a00
Owiz <owiz@protonmail.com>
2024-10-10 14:57:26 +0200

    Update before risky operation

--- Notes.sh
+++ Notes.sh
@@ -1,27 +1,49 @@
-### 1 Permissions
-  sudo -i                   #| Enters root for the current terminal session.
+### 1 System Permissions
+  sudo -i # Enters root for the current terminal session.
 
-### 2 File management
-  dpkg --list *<package>*
-  apt-get -y purge <package>*
-  find / -iname "*<package>*" -exec rm -rf {} \;
-  # Nautilus mounts drives in /media/<user>/
+  # 1.0 Slacken Terminal Password Security
+    sudo visudo # Opens the config for terminal password requests using nano.
+    # Set "%admin    ALL=(ALL:ALL) NOPASSWD:ALL", then Ctrl+Shift+O to save and Ctrl+Shift+X to exit nano.
 
-### 3 Wine
-  # WARNING: Wine is very sensitive, haven't yet found a way to reset (other than to reinstall Linux).
-  winecfg                   #| Config GUI for wine with a few WinOS settings.
-  wine uninstaller          #| Gives overview of successful installations and faciliates formal uninstallations.
-  wineboot                  #| Is supposed to simulate a windows restart, don't know how to verify success.
-  winetricks                #| Additional functionality like wrappers for dependencies. Installed seperately.
+  # 1.1 Slacken GUI Password Security
+  # In "Passwords and Keys" goto Password->Login->Change, and leave it blank to prevent keyring password requests.
+  # If no keyring is created, logging into VS Code will prompt one, or just goto Add->'Password Keyring'.
 
-### 4 ffmpeg
-  ffmpeg -i i.mkv           #| Performs an operation on the specified file, default only prints info.
-    -v <level>              #| Verbosity levels: quiet, panic, fatal, error, warning, info (default), verbose.
-    -vf “crop=X:X:X:X”      #| Specifies cropping for operation.
-    -c:v <codec>            #| Specifies video encoding, for ex. libx265 for CPU, h265_nvenc for GPU. CPU might
-                            #| have higher compression efficiency compared to file size, according to ChatGPT.
-      -preset 6             #| 265/264: Sets compression efficiency, ultrafast (0) to veryslow (8), at default 5.
-      -crf 15               #| 265/264: Constant Rate Factor sets compression quality from 1 to 51, at default 28.
-    -c:a <codec>            #| Specifies audio encoding, normally "-c:a copy". At default it matches video codec.
-  ffplay -i i.mkv           #| Plays the specified file.
-    -vf cropdetect          #| Continuously prints detected stream black bars as "crop=3840:1920:0:120".
+### 2 Other System Commands
+  # FYI: Nautilus mounts drives in /media/<user>/
+  dpkg --get-selections | grep <package>
+  dconf dump / > dconfdump # Saves a bunch of gnome-managed settings to file 'dconfdump'.
+  dconf load / < dconfdump # Loads the gnome-managed settings of file 'dconfdump'.
+  lsb_release -a # Prints the OS version
+
+### 3 App Installation
+  # 3.0 Node.js & TypeScript
+    # Install Node Version Manager (nvm) as described on github.com/nvm-sh/nvm
+    nvm install <version> # Specifying major version seems to get Node LTS.
+    apt -y install node-typescript
+
+  # 3.1 Beekeeper Studio
+    flatpak -y install flathub io.beekeeperstudio.Studio
+    sudo flatpak override io.beekeeperstudio.Studio --filesystem=host
+
+### 4 App Usage
+  # 4.0 Wine
+    # WARNING: Wine is very sensitive, haven't yet found a way to reset (other than to reinstall Linux).
+    winecfg               #| Config GUI for wine with a few WinOS settings.
+    wine uninstaller      #| Gives overview of successful installations and faciliates formal uninstallations.
+    wineboot              #| Is supposed to simulate a windows restart, don't know how to verify success.
+    winetricks            #| Additional functionality like wrappers for dependencies. Installed seperately.
+    apt install zorin-windows-app-support # Wine & Bottle helper for ZorinOS (efficacy unknown).
+                          #| Zorin comes with an installation shortcut, "Windows App Support" in the start menu.
+  
+  # 4.1 ffmpeg
+    ffmpeg -i i.mkv       #| Performs an operation on the specified file, default only prints info.
+      -v <level>          #| Verbosity levels: quiet, panic, fatal, error, warning, info (default), verbose.
+      -vf “crop=X:X:X:X”  #| Specifies cropping for operation.
+      -c:v <codec>        #| Specifies video encoding, for ex. libx265 for CPU, h265_nvenc for GPU. CPU might
+                          #| have higher compression efficiency compared to file size, according to ChatGPT.
+        -preset 6         #| 265/264: Sets compression efficiency, ultrafast (0) to veryslow (8), at default 5.
+        -crf 15           #| 265/264: Constant Rate Factor sets compression quality from 1 to 51, at default 28.
+      -c:a <codec>        #| Specifies audio encoding, normally "-c:a copy". At default it matches video codec.
+    ffplay -i i.mkv       #| Plays the specified file.
+      -vf cropdetect      #| Continuously prints detected stream black bars as "crop=3840:1920:0:120".
--- Setup.sh
+++ Setup.sh
@@ -1,100 +1,86 @@
-#!/bin/bash -u
+[ "$EUID" -eq 0 ] && echo -e "$0: Do not use script as root!" >&2 && exit $EXIT_FAILURE
+[ $0 != 'bash' ] && echo -e "$0: Source the script with bash, don't run it!" >&2 && exit $EXIT_FAILURE
 
-ans=''
+### 0 Declarations
+  # 0.0 General
+    dir=$(dirname "${BASH_SOURCE[0]}")
+    ans=''
 
-### 1 Permissions
-  sudo visudo # Opens the config for terminal password requests using nano.
-  # Set "%admin    ALL=(ALL:ALL) NOPASSWD:ALL", then Ctrl+Shift+O to save and Ctrl+Shift+X to exit nano.
-  # In "Passwords and Keys" goto Password->Login->Change, and leave it blank to prevent keyring password requests.
-  sudo apt-mark manual gdm3 gnome-shell gnome-browser-connector # Protects the specified packages from auto-removal.
+  # 0.1 Printing
+    NC='\033[0m'
+    R='\033[0;31m'
+    B='\033[0;34m'
+    bf=$(tput bold)
+    nf=$(tput sgr0)
+    section() {
+      echo -e "$B$bf$@$nf$nc"
+    }
 
-### 2 Uninstalls
-    list=(
-      brasero brasero-common nautilus-extension-brasero
-      cheese
-      eog
-      evince
-      evolution evolution-data-server
-      geary
-      gnome-calendar
-      gnome-characters
-      gnome-clocks
-      gnome-contacts
-      gnome-font-viewer
-      gnome-logs
-      gnome-photos
-      gnome-sound-recorder
-      gnome-weather
-      libreoffice-common libreoffice-core libreoffice-style-yaru
-      remmina
-      rhythmbox
-      totem
-      zorin-connect gnome-shell-extension-zorin-connect gnome-shell-extension-zorin-connect-browsers
-      zorin-os-upgrader
-    )
-    
-    sudo apt purge ${list[@]} # Read before approving operation.
-    echo -e '\nThese similar packages were NOT REMOVED:'
-    sudo dpkg --get-selections | grep -E "($(IFS="|"; echo "${list[*]}"))"
-    echo -e '\nPress [Ctrl+C] to cancel and [Enter] to continue.' && read
-
-### 3 Installs
-  # 3.0 Updates
-  sudo apt -y update && sudo apt -y dist-upgrade
-  fwupdmgr get-devices >/dev/null
-  fwupdmgr get-updates && fwupdmgr -y update
-  flatpak update
-  rm -rf ~/.cache/gnome-software
+### 1 Uninstalls
+  list=(
+    brasero brasero-common nautilus-extension-brasero
+    cheese
+    eog
+    evince
+    evolution evolution-data-server
+    geary
+    gnome-calendar
+    gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-photos
+    gnome-sound-recorder
+    gnome-weather
+    libreoffice-common libreoffice-core libreoffice-style-yaru
+    remmina
+    rhythmbox
+    totem
+    zorin-connect gnome-shell-extension-zorin-connect gnome-shell-extension-zorin-connect-browsers
+    zorin-os-upgrader
+  )
   
-  # 3.1 Standard Apps
-  snap install --classic code
-  snap install spotify
-  flatpak -y install flathub org.gnome.Loupe com.github.rafostar.Clapper
-  apt -y install git ffmpeg
-  git config --system user.name Owiz
-  git config --system user.email owiz@protonmail.com
-  
-  # 3.2 Optional Apps
-  while [[ ! $ans =~ (y|n) ]]; do read -p 'Install Steam (y/n): ' ans; done
-  if [[ $ans == 'y' ]]; then
-    flatpak -y install flathub com.valvesoftware.Steam
-    apt -y install steam-devices
-  fi
-  
-  # 3.3 Clean
-  sudo apt -y autoremove
-  sudo apt clean
-  read -p 'Press [Ctrl+C] to cancel and [Enter] to reboot system.' && sudo reboot now
+  section '\nUninstalling Apps'
+  sudo apt purge ${list[@]} # Read before approving operation.
+  echo -e '\nThese similar packages were NOT REMOVED:'
+  sudo dpkg --get-selections | grep -E "($(IFS="|"; echo "${list[*]}"))"
+  echo -e 'Press [Ctrl+C] to cancel and [Enter] to continue.' && read
 
-### 4 Setup Software
-  # 4.0 Firefox
-  # Copy profile files into ~/.mozilla/firefox/xxxxxxxx.default-release/
-
-  # 4.1 Python
-    apt -y install python3 # Consider using a pre-installed version, Ubuntu has Python among its dependencies.
+### 2 Installs
+  # 2.0 Updates
+    section 'Updating System'
+    echo 'Looking for updates...'
+    sudo apt -qqy update && sudo apt -qqy dist-upgrade
+    flatpak update
+    fwupdmgr get-devices >/dev/null
+    fwupdmgr get-updates && fwupdmgr -y update
+    rm -rf ~/.cache/gnome-software
+  
+  # 2.1 Standard Apps
+    section '\nInstalling Apps'
+    sudo snap install --classic code
+    sudo snap install spotify
+    flatpak -y install flathub org.gnome.Loupe com.github.rafostar.Clapper
+    sudo apt -y install git ffmpeg python3 python3-pip firefox
     echo "alias py=python3" >> ~/.bashrc
-    source ~/.bashrc
-
-  # 4.2 Node.js & TypeScript
-    # Install Node Version Manager (nvm) as described on github.com/nvm-sh/nvm
-    nvm install <version> # Specifying major version seems to get Node LTS.
-    apt -y install node-typescript
-
-### 5 Zorin 17
-  # 5.0 WinTile
-    # Goto extensions.gnome.org and install WinTile.
-    # Drag using Ctrl to individual tiles and Ctrl+Super to combined tiles.
-    # Seems WinTile reconfigures Ctrl+Super+Arrow hotkeys.
-
-  # 5.1 Steam
-    # Get Steam and steam-devices via software manager.
-    # Start controllers before starting games. Update controllers via Windows.
-
-  # 5.2 Windows Apps
-    # Try "Windows App Support" in software manager.
-
-### 6 Miscellaneous
-  # 6.0 Shortcuts
-    chmod +x ~/Documents/GitHub/Bash-scripts/Toggle_GNOME-topbar.sh
-    # Add keyboard shortcut "bash /home/admin/Documents/GitHub/Bash-scripts/Toggle_GNOME-topbar.sh".
-    # Install dconf Editor and set /org/desktop/wm/keybindings/always-on-top to ['<Super>A'].
+    git config --global user.name Owiz
+    git config --global user.email owiz@protonmail.com
+    pip -q install numpy gnome-extensions-cli
+    gext install wintile@nowsci.com # Drag using Ctrl to individual tiles and Ctrl+Super to combined tiles.
+    dconf load / < "$dir/dconf_backup"
+    firefox_profile=$(grep 'Path=.*default-release' ~/.mozilla/firefox/profiles.ini | sed s/^Path=//)
+    cp -a "$dir/Firefox/." ".mozilla/firefox/$firefox_profile/" # Currently: Bookmark icons & cookie-exception settings
+  
+  # 2.2 Optional Apps
+    while [[ ! $ans =~ (y|n) ]]; do read -p 'Install Steam? [Y/n] ' ans; done
+    if [[ $ans == 'y' ]]; then
+      flatpak -y install flathub com.valvesoftware.Steam
+      sudo apt -qqy install steam-devices
+    fi
+  
+  # 2.3 Clean
+    section '\nCleaning Up'
+    sudo apt -y autoremove
+    sudo apt clean
+    read -p 'Press [Ctrl+C] to cancel and [Enter] to reboot system.' && sudo reboot now
